19d06c31e25e9d7fcaa9dba57b89a681dc690645
Haris Khan <haris@onekhan.com>
2026-02-22 00:22:52 -0500

    feat: add native macOS About dialog and fix menu bar name
    Add mac.Options.About for a native About dialog showing version.
    Set info.productName in wails.json so the menu bar shows "Goldsplit"
    instead of lowercase "goldsplit".

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
 )
 
 //go:embed all:frontend/dist
@@ -32,6 +33,12 @@ func main() {
 			Assets: assets,
 		},
 		BackgroundColour: &options.RGBA{R: 18, G: 18, B: 24, A: 1},
+		Mac: &mac.Options{
+			About: &mac.AboutInfo{
+				Title:   "Goldsplit",
+				Message: "Version " + version,
+			},
+		},
 		OnStartup:        app.startup,
 		OnShutdown:       app.shutdown,
 		Bind: []interface{}{
